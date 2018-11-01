61adfbc1514843ca1cb1ded4ac463aca8ed39a09
JoshyJosh <herukula@gmail.com>
2018-11-01 12:42:48 +0100

    fixed connection closed error by running client.close() in loop

--- main2.py
+++ main2.py
@@ -6,16 +6,16 @@ import aiohttp
 import json
 
 loop = asyncio.get_event_loop()
-client = aiohttp.ClientSession(loop=loop)
+conn = aiohttp.TCPConnector()
+client = aiohttp.ClientSession(loop=loop, connector=conn)
 
 asyncdict = {'python': False, 'programming': False, 'compsci': False}
 
 async def get_json(client, url):
     async with client.get(url) as response:
         assert response.status == 200
-        data = response.read()
-        # response.close()
-        return await data
+        data = await response.read()
+        return data
 
 async def get_reddit_top(subreddit, client):
     data1 = await get_json(client, 'https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=day&limit=5')
@@ -29,15 +29,11 @@ async def get_reddit_top(subreddit, client):
         result_string = str(score) + ': ' + title + ' (' + link + ')'
         result_strings.append(result_string)
 
-    print('DONE:', subreddit + '\n')
+    print('\nDONE:', subreddit + '\n')
+    print(result_strings)
 
-    asyncdict[subreddit] = True
     return result_strings
 
-async def check_completed():
-    print("made it here")
-    if asyncdict.count(True) == 3:
-        import pdb; pdb.set_trace()
 
 task1 = asyncio.gather(get_reddit_top('python', client))
 task2 = asyncio.gather(get_reddit_top('programming', client))
@@ -46,42 +42,15 @@ task3 = asyncio.gather(get_reddit_top('compsci', client))
 
 all_tasks = asyncio.gather(task1, task2, task3)
 
-results = loop.run_until_complete(all_tasks)
+try:
+    results = loop.run_until_complete(all_tasks)
+finally:
+    # Gracefully close connection
+    loop.run_until_complete(client.close())
+    conn.close()
+    loop.close()
 
-client.close()
 
-loop.close()
 
-print(results)
-
-#
-# async def get_reddit_top(subreddit, client):
-#     data1 = await get_json(client, 'https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=day&limit=5')
-#
-#     j = json.loads(data1.decode('utf-8'))
-#     for i in j['data']['children']:
-#         score = i['data']['score']
-#         title = i['data']['title']
-#         link = i['data']['url']
-#         print(str(score) + ': ' + title + ' (' + link + ')')
-#
-#     print('DONE:', subreddit + '\n')
-#
-# # Takes keyboard signal and runs the following method
-# def signal_handler(signal, frame):
-#     loop.stop()
-#     client.close()
-#     sys.exit(0)
-#
-# # overrides SIGINT (KeyboardInterrupt)
-# signal.signal(signal.SIGINT, signal_handler)
-#
-#
-# # TODO make this async
-# async def all_of_my_asyncs():
-#     task1 = get_reddit_top('python', client)
-#     task2 = get_reddit_top('programming', client)
-#     task3 = get_reddit_top('compsci', client)
-#
-#
-# loop.run_forever()
+# print(results)
+print("script ended")
