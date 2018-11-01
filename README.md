c3ee03b822e1bdbb27533e891a372256b681c84e
JoshyJosh <herukula@gmail.com>
2018-11-01 12:53:47 +0100

    Renamed file and added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# An asyncio and aiohttp example
+
+This was adapted from an example posted on [Stack Abuse](https://stackabuse.com/python-async-await-tutorial/).
+
+I wanted to make a loop that did the same concurrent calls, but didn't need to be manually stopped.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Taken from https://stackabuse.com/python-async-await-tutorial/
+# Adapted from https://stackabuse.com/python-async-await-tutorial/
 import signal
 import sys
 import asyncio
@@ -6,37 +6,48 @@ import aiohttp
 import json
 
 loop = asyncio.get_event_loop()
-client = aiohttp.ClientSession(loop=loop)
+conn = aiohttp.TCPConnector()
+client = aiohttp.ClientSession(loop=loop, connector=conn)
+
+asyncdict = {'python': False, 'programming': False, 'compsci': False}
+
 
 async def get_json(client, url):
     async with client.get(url) as response:
         assert response.status == 200
-        return await response.read()
+        data = await response.read()
+        return data
+
 
 async def get_reddit_top(subreddit, client):
     data1 = await get_json(client, 'https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=day&limit=5')
 
     j = json.loads(data1.decode('utf-8'))
+    result_strings = []
     for i in j['data']['children']:
         score = i['data']['score']
         title = i['data']['title']
         link = i['data']['url']
-        print(str(score) + ': ' + title + ' (' + link + ')')
+        result_string = str(score) + ': ' + title + ' (' + link + ')'
+        result_strings.append(result_string)
 
-    print('DONE:', subreddit + '\n')
+    print('\nDONE:', subreddit + '\n')
+    print(result_strings)
 
-# Takes keyboard signal and runs the following method
-def signal_handler(signal, frame):
-    loop.stop()
-    client.close()
-    sys.exit(0)
+    return result_strings
 
-# overrides SIGINT (KeyboardInterrupt)
-signal.signal(signal.SIGINT, signal_handler)
 
-asyncio.ensure_future(get_reddit_top('python', client))
-asyncio.ensure_future(get_reddit_top('programming', client))
-asyncio.ensure_future(get_reddit_top('compsci', client))
+task1 = asyncio.gather(get_reddit_top('python', client))
+task2 = asyncio.gather(get_reddit_top('programming', client))
+task3 = asyncio.gather(get_reddit_top('compsci', client))
+# foo4 = asyncio.ensure_future(check_completed())
 
-# loop.run_until_complete() needs a future for an argument check main2.py
-loop.run_forever()
+all_tasks = asyncio.gather(task1, task2, task3)
+
+try:
+    results = loop.run_until_complete(all_tasks)
+finally:
+    # Gracefully close connection
+    loop.run_until_complete(client.close())
+    conn.close()
+    loop.close()
